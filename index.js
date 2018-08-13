e18e8577a0a6bd4eb395e958e55659d2854dcdd7
Jobayer Arman <jobayer.arman@gmail.com>
2018-08-14 04:21:31 +0600

    added bootbot

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const BootBot       = require('bootbot');
+const PORT          = process.env.PORT || 5000;
+
+const bot = new BootBot({
+  accessToken: process.env.access_token,
+  verifyToken: process.env.verify_token,
+  appSecret: process.env.app_secret
+});
+
+bot.on('message', (payload, chat,) => {
+    chat.sendTypingIndicator(1000)
+    .then(() => {
+      chat.say(['Welcome to Aso Chat Kori', 'I\'m a program', 'I was written by Womee']);
+    });
+});
